0e47e4db5491e2d67774e5b9ec1d2fc8f9205283
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-08-22 11:52:56 +0600

    ds 1

--- 3rd Semester/Data Structure/lab_1/2.cpp
+++ 3rd Semester/Data Structure/lab_1/2.cpp
@@ -6,11 +6,32 @@
 using namespace std;
 
 int main() {
-    
 
+    int arr[] = {1, 2, 3, 5, 5, 7, 8, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    int count = 0;
 
+    cout << "Array: ";
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
 
+    int arr2[100000] = {0};
+    for(int i=0; i<size; i++){
+        arr2[arr[i]]++;
+    }
 
-    
+    for(int i=0; i<100000; i++){
+        int go = target - i;
+        if(arr2[go] > 0){
+            count++;
+            cout << "\n" << i << " + " << go << " = " << target << endl;
+            arr2[go]--;
+        }
+    }
+
+    cout << "\nTotal pairs: " << count << endl;
+
     return 0;
 }
