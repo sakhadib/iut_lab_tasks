1c480e398c32ebd2366b8ad181a66d08b2a77567
sakhadib <adibsakhawat@iut-dhaka.edu>
2023-10-26 10:37:21 +0600

    oop lab 9

---
+++ 3rd Semester/OOP_SWE_4302/Lab_9/ConsoleApp1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+
+        public Person(string Name, string address, int age)
+        {
+            this.Name = Name;
+            this.address = address;
+            this.age = age;
+        }
+    }
+}
---
+++ 3rd Semester/OOP_SWE_4302/Lab_9/Shape/TwoD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class TwoD : Shape
+    {
+        public TwoD() { }
+    }
+}
